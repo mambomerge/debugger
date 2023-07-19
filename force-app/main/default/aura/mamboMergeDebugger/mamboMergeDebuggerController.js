9d4ee771d4a110a9412bac4b17535fe49a03ad9c
Kevin Kuo <kevinkuo0905@hotmail.com>
2023-07-19 14:58:02 -0700

    Update mamboMergeDebuggerController.js

--- force-app/main/default/aura/mamboMergeDebugger/mamboMergeDebuggerController.js
+++ force-app/main/default/aura/mamboMergeDebugger/mamboMergeDebuggerController.js
@@ -2,7 +2,7 @@
   toggleMamboMerge: function (component, event, helper) {
     let s = component.get('v.mergeDataString');
     let j = JSON.parse(s);
-    component.set('v.mergeData', [j]);
+    component.set('v.mergeData', j);
 
     let showMambo = !component.get('v.showMambo');
     console.log(showMambo);
